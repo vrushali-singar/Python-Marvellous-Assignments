cab5843ebf467080d4f8346b84058d3b23f2d926
vrushali-singar <147076875+vrushali-singar@users.noreply.github.com>
2025-05-11 14:10:07 +0530

    Delete Assignments/Assignment1 directory

--- Assignments/Assignment1/Assignmnet1_1.py
+++
@@ -1,7 +0,0 @@
-#Write a program which contains one function named as Fun(). 
-#That function should display "Hello from Fun" on console.
-
-def fun():   #function define
-    print("Hello from Fun")
-
-fun() #call to function
